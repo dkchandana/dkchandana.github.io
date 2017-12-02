5cf76fff0e836f372bf041fb2394567cf7bab391
dkchandana <34017367+dkchandana@users.noreply.github.com>
2017-12-02 09:44:51 -0600

    Commit#0

---
+++ Vensi/main.js
@@ -0,0 +1,35 @@
+function switchON(){
+    alert("Inside Connect");
+	let options = {};
+	options.acceptAllDevices = true;
+            return navigator.bluetooth.requestDevice(options)//requestDevice({filters:[{services:['SATECHIPLUG']}]})
+                .then(device => {
+                console.log(device.name);
+                console.log('Here-esrvice');
+                this.device = device;
+                return device.gatt.connect();
+            })
+                .then(server => {
+                this.server = server;
+                return Promise.all([
+                    server.getPrimaryService('0000fff0-0000-1000-8000-00805f9b34fb') //Replace the service value
+                .then(
+				
+				service => {   
+                    return service.getCharacteristic('0000fff1-0000-1000-8000-00805f9b34fb'); // replace charecteristic
+						 })
+				.then(function(characteristic) {
+                        console.log('Its here in charecteristic');
+                        return characteristic.writeValue(0x002b);
+                    })
+                ]);
+            })
+                .catch(function(error) {
+                // And of course: error handling!
+                console.error('Connection failed!', error);
+            })
+
+}
+function switchOFF(){
+     alert("Inside Off");
+}
