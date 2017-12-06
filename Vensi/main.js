aafa8c832c965f338873dea7a61144eafb5f1fa3
dkchandana <34017367+dkchandana@users.noreply.github.com>
2017-12-06 11:47:37 -0600

    Commit 2

--- Vensi/main.js
+++ Vensi/main.js
@@ -1,35 +1,165 @@
-function switchON(){
+function switchOFF(){
     alert("Inside Connect");
-	let options = {};
+	let options = {optionalServices: ['0000fff0-0000-1000-8000-00805f9b34fb']};
 	options.acceptAllDevices = true;
-            return navigator.bluetooth.requestDevice(options)//requestDevice({filters:[{services:['SATECHIPLUG']}]})
+    var value = new Uint8Array([0x0F, 0x06, 0x03, 0x00, 0x00, 0x00, 0x00, 0x04, 0xFF, 0xFF]);
+	console.log(value);
+    //value[0] = 0x002b;
+    //BluetoothGattCharacteristic charac = null;
+	//charac.setValue(value);
+				
+			return navigator.bluetooth.requestDevice(options)//requestDevice({filters:[{services:['SATECHIPLUG']}]})
                 .then(device => {
                 console.log(device.name);
-                console.log('Here-esrvice');
+                console.log('In-service');
                 this.device = device;
                 return device.gatt.connect();
-            })
+						})
                 .then(server => {
                 this.server = server;
                 return Promise.all([
                     server.getPrimaryService('0000fff0-0000-1000-8000-00805f9b34fb') //Replace the service value
-                .then(
-				
-				service => {   
-                    return service.getCharacteristic('0000fff1-0000-1000-8000-00805f9b34fb'); // replace charecteristic
+                .then( service => {   
+                return service.getCharacteristic('0000fff3-0000-1000-8000-00805f9b34fb'); // replace charecteristic
 						 })
 				.then(function(characteristic) {
-                        console.log('Its here in charecteristic');
-                        return characteristic.writeValue(0x002b);
-                    })
+                 console.log('In charecteristic');
+				 console.log(characteristic);
+				 console.log(value);
+                 return characteristic.writeValue(value);  //The ON-OFF Command
+						})
                 ]);
-            })
+            }) 
+		
                 .catch(function(error) {
                 // And of course: error handling!
                 console.error('Connection failed!', error);
             })
 
 }
-function switchOFF(){
-     alert("Inside Off");
+
+
+function switchON(){
+    alert("Inside Connect");
+	let options = {optionalServices: ['0000fff0-0000-1000-8000-00805f9b34fb']};
+	options.acceptAllDevices = true;
+    var value = new Uint8Array([0x0F, 0x06, 0x03, 0x00, 0x01, 0x00, 0x00, 0x05, 0xFF, 0xFF]);
+	console.log(value);
+    //value[0] = 0x002b;
+    //BluetoothGattCharacteristic charac = null;
+	//charac.setValue(value);
+				
+			return navigator.bluetooth.requestDevice(options)//requestDevice({filters:[{services:['SATECHIPLUG']}]})
+                .then(device => {
+                console.log(device.name);
+                console.log('In-service');
+                this.device = device;
+                return device.gatt.connect();
+						})
+                .then(server => {
+                this.server = server;
+                return Promise.all([
+                    server.getPrimaryService('0000fff0-0000-1000-8000-00805f9b34fb') //Replace the service value
+                .then( service => {   
+                return service.getCharacteristic('0000fff3-0000-1000-8000-00805f9b34fb'); // replace charecteristic
+						 })
+				.then(function(characteristic) {
+                 console.log('In charecteristic');
+				 console.log(characteristic);
+				 console.log(value);
+                 return characteristic.writeValue(value);  //The ON-OFF Command
+						})
+                ]);
+            }) 
+		
+                .catch(function(error) {
+                // And of course: error handling!
+                console.error('Connection failed!', error);
+            })
+
+}
+
+
+function switchREAD(){
+     alert("Inside Read");
+	 
+	let options = {optionalServices: ['0000fff0-0000-1000-8000-00805f9b34fb']};
+	options.acceptAllDevices = true;
+    			
+			return navigator.bluetooth.requestDevice(options)//requestDevice({filters:[{services:['SATECHIPLUG']}]})
+                .then(device => {
+                console.log(device.name);
+                console.log('In-service');
+                this.device = device;
+                return device.gatt.connect();
+						})
+				.then(server => {
+					console.log('Getting Service...');
+					return server.getPrimaryService('0000fff0-0000-1000-8000-00805f9b34fb');
+					})
+				.then(service => {
+					console.log('Getting Characteristic...');
+					return service.getCharacteristic('0000fff4-0000-1000-8000-00805f9b34fb');
+				  })
+				.then(characteristic => {
+					myCharacteristic = characteristic;
+					return myCharacteristic.startNotifications().then(_ => {
+					  console.log('> Notifications started');
+					  myCharacteristic.addEventListener('characteristicvaluechanged',
+						  handleNotifications);
+					});
+				  })
+				 .catch(function(error) {
+                // And of course: error handling!
+                console.error('Connection failed!', error);
+            })
+}
+
+
+function switchSRVCHAR() {
+  alert("Inside SRVCHAR");
+  log('Requesting any Bluetooth Device...');
+  navigator.bluetooth.requestDevice({
+   // filters: [...] <- Prefer filters to save energy & show relevant devices.
+      acceptAllDevices: true,
+      optionalServices: optionalServices})
+  .then(device => {
+    log('Connecting to GATT Server...');
+    return device.gatt.connect();
+  })
+  .then(server => {
+    // Note that we could also get all services that match a specific UUID by
+    // passing it to getPrimaryServices().
+    log('Getting Services...');
+    return server.getPrimaryServices();
+  })
+  .then(services => {
+    log('Getting Characteristics...');
+    let queue = Promise.resolve();
+    services.forEach(service => {
+      queue = queue.then(_ => service.getCharacteristics().then(characteristics => {
+        log('> Service: ' + service.uuid);
+        characteristics.forEach(characteristic => {
+          log('>> Characteristic: ' + characteristic.uuid + ' ' +
+              getSupportedProperties(characteristic));
+        });
+      }));
+    });
+    return queue;
+  })
+  .catch(error => {
+    log('Argh! ' + error);
+  });
+}
+
+/* Utils */
+
+function getSupportedProperties(characteristic) {
+  let supportedProperties = [];
+  for (const p in characteristic.properties) {
+    if (characteristic.properties[p] === true) {
+      supportedProperties.push(p.toUpperCase());
+    }
+  }
+  return '[' + supportedProperties.join(', ') + ']';
 }
